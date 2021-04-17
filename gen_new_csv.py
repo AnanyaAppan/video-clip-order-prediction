61abb6278a12e391a961e74837344ed82f6716f3
Ananya <ananya.appan@gmail.com>
2021-04-17 18:19:14 +0530

    added python code to modify csv file to remove videos not present

---
+++ gen_new_csv.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import os.path
+from os import path
+
+root = "/home/hdd2/ananya/Autism/ActivityNet/Crawler/Kinetics/"
+data = pd.read_csv(root + "data/kinetics-600_train.csv")
+for index, row in data.iterrows():
+    label = row["label"]
+    videoname = row["youtube_id"]
+    start_time = row["time_start"]
+    end_time = row["time_end"]
+    videofile = videoname+'_'+str(start_time).zfill(6)+'_'+str(end_time).zfill(6)+".mp4"
+    filename = os.path.join(self.root_dir, 'dataset', label, videofile)
+    if not path.exists(filename):
+        data.drop(index, inplace=True)
+data.to_csv(root + "data/kinetics-600_train_modified.csv")
