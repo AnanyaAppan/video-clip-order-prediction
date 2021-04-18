720d217a121b25a3111fa9ac3925df93063d5306
Ananya <ananya.appan@gmail.com>
2021-04-18 13:03:15 +0530

    reshape 4096 to 1x4096

--- models/i3d.py
+++ models/i3d.py
@@ -323,7 +323,7 @@ class InceptionI3d(nn.Module):
         for end_point in self.VALID_ENDPOINTS:
             if end_point in self.end_points:
                 x = self._modules[end_point](x) # use _modules to work with dataparallel
-        return self.avg_pool(x) # return extracted features
+        return torch.reshape(self.avg_pool(x),(1,4096)) # return extracted features
 
         x = self.logits(self.dropout(self.avg_pool(x)))
         if self._spatial_squeeze:
