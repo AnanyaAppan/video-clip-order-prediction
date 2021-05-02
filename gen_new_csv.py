01153104a02ec78ff34005cf4313583d48da75ea
Ananya <ananya.appan@gmail.com>
2021-05-02 22:48:10 +0530

    new gen 50 code

--- gen_new_csv.py
+++ gen_new_csv.py
@@ -18,6 +18,7 @@ for index, row in data.iterrows():
         data.drop(index, inplace=True)
     else : num += 1
     if(num == 50): break
+data = data[:50]
 data.to_csv("./kinetics-600_train_50.csv")
 
 # data = pd.read_excel("req_classes.ods")
