8cd6ffd36e60a4ee7bf38dfebe67e522fabb9e4e
Ananya <ananya.appan@gmail.com>
2021-05-08 22:49:07 +0530

    trying to train entire network on 12 classes

--- train_vcop.py
+++ train_vcop.py
@@ -192,9 +192,9 @@ if __name__ == '__main__':
     elif args.model == 'i3d':
         base = InceptionI3d(400,in_channels=3)
         base.load_state_dict(torch.load('../pytorch-i3d/models/rgb_imagenet.pt'))
-    for name,param in base.named_parameters():
-        if('Mixed_5c' not in name):
-            param.requires_grad = False
+    # for name,param in base.named_parameters():
+    #     if('Mixed_5c' not in name):
+    #         param.requires_grad = False
     # vcopn = VCOPN(base_network=base, feature_size=512, tuple_len=args.tl).to(device)
     vcopn = VCOPN(base_network=base, feature_size=1024, tuple_len=args.tl).to(device) # for i3d
 
@@ -244,7 +244,7 @@ if __name__ == '__main__':
 
         ### loss funciton, optimizer and scheduler ###
         criterion = nn.CrossEntropyLoss()
-        optimizer = optim.SGD(filter(lambda p: p.requires_grad, vcopn.parameters()), lr=args.lr, momentum=args.momentum, weight_decay=args.wd)
+        optimizer = optim.SGD(vcopn.parameters(), lr=args.lr, momentum=args.momentum, weight_decay=args.wd)
         scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min', min_lr=1e-5, patience=50, factor=0.1)
 
         prev_best_val_loss = float('inf')
