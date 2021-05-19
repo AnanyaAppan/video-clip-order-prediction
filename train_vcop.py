773023b44fef44a300ca4128cbbd501d5cfb975a
Ananya <ananya.appan@gmail.com>
2021-05-19 21:49:28 +0530

    trying to train with c3d

--- train_vcop.py
+++ train_vcop.py
@@ -199,8 +199,8 @@ if __name__ == '__main__':
     # for name,param in base.named_parameters():
     #     if('Mixed_5c' not in name):
     #         param.requires_grad = False
-    # vcopn = VCOPN(base_network=base, feature_size=512, tuple_len=args.tl).to(device)
-    vcopn = VCOPN(base_network=base, feature_size=1024, tuple_len=args.tl).to(device) # for i3d
+    vcopn = VCOPN(base_network=base, feature_size=512, tuple_len=args.tl).to(device)
+    # vcopn = VCOPN(base_network=base, feature_size=1024, tuple_len=args.tl).to(device) # for i3d
 
     if args.mode == 'train':  ########### Train #############
         if args.ckpt:  # resume training
